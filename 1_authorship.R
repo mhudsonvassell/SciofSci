487c67dfdc1833efb8f0f1e38e4c25317cb0544d
Michael Hudson-Vassell <mhudsonvassell@gmail.com>
2023-08-08 11:29:08 -0400

    Reworked shiny app to ensure all inputs can be used to filter for all plots, including optional variables. After importing xlsx, selection variables now mutate to factor. Included blank option for inputs so user has clean slate to begin.

--- 1_authorship.R
+++ 1_authorship.R
@@ -6,23 +6,25 @@ library(tidyverse)
 library(openxlsx)
 
 prop1_age <- read.xlsx("https://raw.github.com/mhudsonvassell/SciofSci/main/prop1_age.xlsx")
-
+prop1_age <- prop1_age %>%
+  mutate_at(c("sex","age","race","subject_lvl1"),as.factor) %>%
+  mutate(across(c("pct","repdiff"),round,2))
 
 ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput("age", "Author age range",
-                  choices = levels(prop1_age$age),
-                  selected = "20-25"),
+                  choices = c("",levels(prop1_age$age)),
+                  selected = NULL),
       selectInput("race", "Author race",
-                  choices = levels(prop1_age$race),
-                  selected = "Black African"),
+                  choices = c("",levels(prop1_age$race)),
+                  selected = NULL),
       selectInput("gender", "Author gender",
-                  choices = levels(prop1_age$sex),
-                  selected = "Female"),
+                  choices = c("",levels(prop1_age$sex)),
+                  selected = NULL),
       selectInput("subject", "Subject",
-                  choices = levels(prop1_age$subject_lvl1),
-                  selected = "{Natural sciences}"),
+                  choices = c("",levels(prop1_age$subject_lvl1)),
+                  selected = NULL),
       sliderInput("years", "For which years?",
                   min(prop1_age$year), max(prop1_age$year),
                   value = c(2004, 2020),round=TRUE,sep="")
@@ -50,8 +52,8 @@ server <- function(input, output) {
                        sex %in% input$gender &
                        age %in% input$age &
                        year >= input$years[1] & year <= input$years[2])
-      
-      ggplot(data, aes(year, totcount)) +
+      if(input$subject!=""){data <- data %>% filter(subject_lvl1 %in% input$subject)}
+      ggplot(data, aes(year, totcount, color = subject_lvl1)) +
         geom_point(position="jitter", show.legend = FALSE, alpha=(1/3)) +
           theme(legend.position='none') +
         ggtitle(paste0("# of Authors by Year ","(Group: ",input$race," ",input$gender," ",input$age,")"))
@@ -66,8 +68,8 @@ server <- function(input, output) {
                        sex %in% input$gender &
                        age %in% input$age &
                        year >= input$years[1] & year <= input$years[2])
-      
-      ggplot(data, aes(year, pct, 
+      if(input$subject!=""){data <- data %>% filter(subject_lvl1 %in% input$subject)}
+      ggplot(data, aes(year, pct, color = subject_lvl1,
                        text = paste0("subject count: ", totcount))) +
         geom_point(position="jitter", show.legend = FALSE, alpha=(1/3)) +
         theme(legend.position='none') +
@@ -78,14 +80,16 @@ server <- function(input, output) {
   output$plot3 <- renderPlotly({
     # Convert the existing ggplot2 to a plotly plot
     ggplotly({
-      data <- subset(prop1_age,
+            data <- subset(prop1_age,
                      race %in% input$race &
-                       sex %in% input$gender &
+                     sex %in% input$gender & 
                        age %in% input$age &
+                       
                        year >= input$years[1] & year <= input$years[2])
+            if(input$subject!=""){data <- data %>% filter(subject_lvl1 %in% input$subject)}
       
-      ggplot(data, aes(year, repdiff, 
-                       text = paste0("subject count: ", repdiff))) +
+      ggplot(data, aes(year, repdiff, color = subject_lvl1,
+                       text = paste0("subject count: ", totcount))) +
         geom_point(position="jitter", show.legend = FALSE, alpha=(1/3)) +
         theme(legend.position='none') +
         ggtitle(paste0("Over/under-representation by Year (Group: ",input$race," ",input$gender," ",input$age,")"))
@@ -95,12 +99,13 @@ server <- function(input, output) {
   output$plot4 <- renderPlotly({
     # Convert the existing ggplot2 to a plotly plot
     ggplotly({
-      data <- subset(prop1_age,
-                     subject_lvl1 %in% input$subject & 
+      data <- subset(prop1_age, subject_lvl1 %in% input$subject & 
                        year >= input$years[1] & year <= input$years[2])
-      
+      if(input$race!=""){data <- data %>% filter(race %in% input$race)}
+      if(input$gender!=""){data <- data %>% filter(sex %in% input$gender)}
+      if(input$age!=""){data <- data %>% filter(age %in% input$age)}
       ggplot(data, aes(x = year, y=lvl1count, color=race,
-                       text = paste0("subgroup:", race_gender_age))) +
+                       text = paste0("subgroup: ", race_gender_age))) +
         geom_point(position="jitter", show.legend = FALSE, alpha=(1/3)) +
         theme(legend.position='none') +
         ggtitle(paste0("# of Authors by Subject and Year (Subject: ",input$subject,")"))
